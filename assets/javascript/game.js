b329bdeffa3283452143b503cefdaa84a51f6a96
Nate Shih <nshih.85@gmail.com>
2016-09-01 13:50:05 -0400

    CSS Tweaks left

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,85 +1,122 @@
 
 
-//VARS
+//VARS============================================
 //number of wins
 var winCount = 0;
 //number of losses
 var lossCount = 0;
 //the target score
-var targetScore = "";
+var targetScore = 0;
 //the current score
-var currentScore = "";
+var currentScore = 0;
 //value of the red rune
-var redVal = "";
+var redVal = 0;
 //value of the blue rune
-var blueVal = "";
+var blueVal = 0;
 //value of the green rune
-var greenVal = "";
+var greenVal = 0;
 //value of the black rune
-var blackVal = "";
-//FUNCTIONS
-$( document ).ready(function() {
+var blackVal = 0;
+
+var regen = document.createElement('audio');
+	regen.setAttribute('src', 'assets/sounds/regen.mp3');
+
+var haste = document.createElement('audio');
+	haste.setAttribute('src', 'assets/sounds/haste.mp3');
+
+var dd = document.createElement('audio');
+	dd.setAttribute('src', 'assets/sounds/dd.mp3');
+
+var invis = document.createElement('audio');
+	invis.setAttribute('src', 'assets/sounds/invis.mp3');
+
+var win = document.createElement('audio');
+	win.setAttribute('src', 'assets/sounds/win.mp3');
+
+var lose = document.createElement('audio');
+	lose.setAttribute('src', 'assets/sounds/lose.mp3');
+
+var music = document.createElement('audio');
+	music.setAttribute('src', 'assets/sounds/music.mp3');
+
+//FUNCTIONS=======================================
+
+
+
+$(document).ready(function() {
+		var currentScore = 0;
+		var targetScore = Math.floor(Math.random()*101 + 19);
+		var greenVal = Math.floor(Math.random()*12+1);
+		var redVal = Math.floor(Math.random()*12+1);
+		var blueVal = Math.floor(Math.random()*12+1);
+		var blackVal = Math.floor(Math.random()*12+1);
+		$('#targetNum').html(targetScore);
+		$('#green').attr("data-num", greenVal)
+		$('#red').attr("data-num", redVal)
+		$('#blue').attr("data-num", blueVal)
+		$('#black').attr("data-num", blackVal)
+
+	music.play();
+	
+	$("#green").on("click", function(){
+    regen.play();
+	});
+
+	$("#red").on("click", function(){
+    haste.play();
+	});
+
+	$("#blue").on("click", function(){
+    dd.play();
+	});
+
+	$("#black").on("click", function(){
+    invis.play();
+	});
+
+
 //sets player's current score to 0, generates random values for the target score between 19 and 120 and each rune between 1 and 12
-	var currentScore = 0;
-	var targetScore = Math.floor(Math.random()*101 + 19);
-	var greenVal = Math.floor(Math.random()*12+1);
-	var redVal = Math.floor(Math.random()*12+1);
-	var blueVal = Math.floor(Math.random()*12+1);
-	var blackVal = Math.floor(Math.random()*12+1);
-	$('#targetNum').html(targetScore);
-	console.log(targetScore)
-	console.log(greenVal)
-	console.log(redVal)
-	console.log(blueVal)
-	console.log(blackVal)
-	$('#green').attr("data-num", greenVal)
-	$('#red').attr("data-num", redVal)
-	$('#blue').attr("data-num", blueVal)
-	$('#black').attr("data-num", blackVal)
-	console.log($('#green').data('num'));
-	console.log($('#red').data('num'));
-	console.log($('#blue').data('num'));
-	console.log($('#black').data('num'));
+	function newGame() {
+		currentScore = 0;
+		$('#playerNum').html(currentScore);
+		targetScore = Math.floor(Math.random()*101 + 19);
+		greenVal = Math.floor(Math.random()*12+1);
+		redVal = Math.floor(Math.random()*12+1);
+		blueVal = Math.floor(Math.random()*12+1);
+		blackVal = Math.floor(Math.random()*12+1);
+		$('#targetNum').html(targetScore);
+		$('#green').attr("data-num", greenVal)
+		$('#red').attr("data-num", redVal)
+		$('#blue').attr("data-num", blueVal)
+		$('#black').attr("data-num", blackVal)
+	
+	} 
 
 	//activates when a rune is pressed
 	$('.button').on('click', function() {
-	currentScore = currentScore + $(this).data('num');
-	console.log(currentScore)
-	$('#playerNum').html(currentScore);
-	
-		if (currentScore == targetScore) {
-			alert("You Win!")
-			winCount++
-			$('#wins').html(winCount);
 
-		}
-		else if (currentScore > targetScore) {
-			alert("You Lose.")
-			lossCount++
-			$('#losses').html(lossCount);
+		currentScore += $(this).data('num');
+		$('#playerNum').html(currentScore);
+		
+			if (currentScore == targetScore) {
+				win.play();
+				alert("You Win!")
+				winCount++
+				$('#wins').html(winCount);
+				newGame();
 
-		}
-	});
+			}//Closes Win
 
-});
+			if (currentScore > targetScore) {
+				lose.play();
+				alert("You Lose.")
+				lossCount++
+				$('#losses').html(lossCount);
+				newGame();
 
-//Example from lottery program======================
-// $(document).ready(function(){
+			}//Closes loss
 
-//     // CREATE THE MISSING CODE HERE. Your code should add content to the randomNumber div.
-//     // ...
+	});//Closes button.on(click)
 
+}); //Closes document.ready(function)
 
-//     $('#randomButton').on("click", function(){
-//         var randomNum = Math.floor(Math.random()*(900000000) + 100000000);
-//         var newDiv = $('<div>')
-//         $('#randomNumber').prepend("<br><hr>" + randomNum)
-
-//     })
-
-
-    // ...===============================
-
-// })
-
-//PROGRAM
